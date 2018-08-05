be1ad9990a6f747a63e37de56fbd6db718b0cdb6
Douglas Martin <mdgeek1@gmail.com>
2018-08-05 18:04:47 -0400

    Update POM and script.
    Move copy-webjar-resources to after compile phase.
    Update deployment script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-if [[ "$TRAVIS_BRANCH" = 'master' ]] || [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.x$ ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+
+if [[ "$TRAVIS_PULL_REQUEST" != 'false' ]]; then
+  exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" = 'master' ]]; then
+  mvn -V -B -s travis/settings.xml clean deploy -DskipTests
+  exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" =~ ^(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)$ ]]; then
   openssl aes-256-cbc -K $encrypted_b90015baf6e6_key -iv $encrypted_b90015baf6e6_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
-else
-  echo "Branch $TRAVIS_BRANCH not eligible for deployment."
+  exit 0
 fi
+
+echo "Branch $TRAVIS_BRANCH not eligible for deployment."
